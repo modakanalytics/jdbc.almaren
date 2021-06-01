2847c7f6f69157d9d19958865bb573be00571dfa
Daniel Mantovani <daniel.oliveira.mantovani@gmail.com>
2021-06-01 14:44:34 -0300

    add __ID__ column to track failures

--- src/main/scala/com/github/music/of/the/ainur/almaren/jdbc/JDBCConnector.scala
+++ src/main/scala/com/github/music/of/the/ainur/almaren/jdbc/JDBCConnector.scala
@@ -11,6 +11,7 @@ import scalikejdbc._
 import scala.util.{Failure, Success, Try}
 
 final case class JDBCResponse(
+  `__ID__`:String,
   `__URL__`:String,
   `__DRIVER__`:String,
   `__QUERY__`:String,
@@ -20,6 +21,10 @@ final case class JDBCResponse(
 )
 
 private[almaren] case class MainJDBC(url: String, driver: String, query: String, batchSize: Int, user: Option[String], password: Option[String], params: Map[String, String]) extends Main {
+
+  private object Alias {
+    val IdCol = "__ID__"
+  }
 
   override def core(df: DataFrame): DataFrame = {
     logger.info(s"url:{$url}, driver:{$driver}, query:{$query}, batchSize:{$batchSize}, user:{$user}, params:{$params}")
@@ -32,33 +37,40 @@ private[almaren] case class MainJDBC(url: String, driver: String, query: String,
     val result = df.mapPartitions((partition: Iterator[Row]) => {
       Class.forName(driver)
       ConnectionPool.singleton(url, user.getOrElse(""), password.getOrElse(""))
-      partition.grouped(batchSize).map(rows => batchQuery(rows))
+      partition.grouped(batchSize).flatMap(rows => batchQuery(rows))
     })
     result.toDF
   }
 
-  private def batchQuery(rows:Seq[Row]): JDBCResponse = {
+  private def batchQuery(rows:Seq[Row]): Seq[JDBCResponse] = {
+    // The first column of the DataFrame should be __ID__ that's why we skip it with "index = 1" instead of "index = 0"
+    // It's all the rows except the first column that must be __ID__
     val batchParams: Seq[Seq[Any]] = rows.map(row => {
-      (0 to row.size).map(index => row.get(index - 1)).toSeq
+      (0 to row.size).map(index => row.get(index)).toSeq
     }).toSeq
     val startTime = System.currentTimeMillis()
     DB localTx { implicit session =>
       Try { sql"${SQLSyntax.createUnsafely(query)}".batch(batchParams: _*).apply() } match {
-        case Success(data) => JDBCResponse(
-          `__URL__` = url,
-          `__DRIVER__` = driver,
-          `__QUERY__` = query,
-          `__BATCH_SIZE__` = batchSize,
-          `__ELAPSED_TIME__` = System.currentTimeMillis() - startTime)
-        case Failure(error) => {
-          logger.error("Almaren jdbcBatch error", error)
+        case Success(data) =>  batchParams.map(row =>           
           JDBCResponse(
+            `__ID__` = row.head.asInstanceOf[String],
             `__URL__` = url,
             `__DRIVER__` = driver,
             `__QUERY__` = query,
             `__BATCH_SIZE__` = batchSize,
-            `__ELAPSED_TIME__` = System.currentTimeMillis() - startTime,
-            `__ERROR__` = Some(error.getMessage)
+            `__ELAPSED_TIME__` = System.currentTimeMillis() - startTime)
+        )
+        case Failure(error) => {
+          logger.error("Almaren jdbcBatch error", error)
+          batchParams.map(row =>           
+            JDBCResponse(
+              `__ID__` = row.head.asInstanceOf[String],
+              `__URL__` = url,
+              `__DRIVER__` = driver,
+              `__QUERY__` = query,
+              `__BATCH_SIZE__` = batchSize,
+              `__ELAPSED_TIME__` = System.currentTimeMillis() - startTime,
+              `__ERROR__` = Some(error.getMessage))
           )
         }
       }
